04d4783d8c67331d995df6677e26bc5b88eae325
Pubudu Kodikara <pubudu@leafycode.com>
2015-11-06 12:00:40 +0530

    Template level subscriptions

--- client/recipes/Recipes.js
+++ client/recipes/Recipes.js
@@ -1,1 +1,14 @@
-Meteor.subscribe('recipes');
+// Meteor.subscribe('recipes');
+
+Template.Recipes.onCreated(function(){
+    var self = this;
+    self.autorun(function(){
+        self.subscribe('recipes');
+    });
+});
+
+Template.Recipes.helpers({
+    recipes: () => {
+        return Recipes.find({});
+    }
+});
